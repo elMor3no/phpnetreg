a78533c9cece22d5afc39af16b593f819bf9bca3
elMor3no <pmdcuba@gmail.com>
2015-04-09 16:40:58 -0400

    agregando fonts de bootstrap

---
+++ contact.php
@@ -0,0 +1,46 @@
+<?php
+include("navbar.php")
+?>
+<html>
+<head>
+  <title>PHPNETREG - Contacto</title>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="PHPNETREG">
+    <meta name="author" content="Pablo Mestre">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+  <LINK rel="stylesheet" type="text/css" href="css/style-login.css">
+  <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+</head>
+<body>
+
+<div class="container-fluid row">	
+	<div class="col-xs-12 col-md-1"></div>
+	  <div class="col-xs-12 col-md-10">
+      <div class="panel-heading">
+		<h1>PHPNETREG</h1>
+      </div>
+      <br/>	
+	  <div class="col-xs-12 col-md-11">
+		<h2>Autor: Pablo Mestre</h2>
+		<h2>Email: pmdcuba@gmail.com</h2>
+	  </div>
+	  </div>
+	  <div class="col-xs-12 col-md-2"></div>
+       <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script> 
+    <br><br><br>
+</body>
+</html>
+<?php
+include("footer.php")
+?>
